431db829d1a6cf990373bd133ef87effccb7cf67
Cesar Mira Verdeal <cesarmv2005@gmail.com>
2026-04-10 09:46:17 +0200

    Segunda Practica: Cuenta añadida

--- appBanco.java
+++ appBanco.java
@@ -7,5 +7,6 @@ public class appBanco {
         cliente c1 = new cliente("163589", "juan", LocalDate.now());
         System.out.println(c1.getNombre());
         System.out.println(c1);
+        cuenta cuenta1 = new cuenta("126826", "Cesar");
     }
 }
